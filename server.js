173454e466b44c86b5ef4b3c384d43196f522df8
kevorik <bogdanlv@mail.ru>
2022-12-02 17:50:11 +0300

    add

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ server.use(jsonServer.rewriter({
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
 server.use(router)
-server.listen(4000, () => {
+server.listen('https://vercel-pink-nu.vercel.app/heroes/', () => {
     console.log('JSON Server is running')
 })
 
